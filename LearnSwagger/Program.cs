9919458c451baa87a7f088440a3680fe975b8047
Dimon080204 <batyshnyi.dmytro.54@gmail.com>
2025-09-29 15:55:38 +0300

    Swagger is added

--- LearnSwagger/Program.cs
+++ LearnSwagger/Program.cs
@@ -6,12 +6,20 @@ builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new() { Title = "LearnSwagger", Version = "v1" });
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
